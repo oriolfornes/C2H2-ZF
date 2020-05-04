09e64a7d3db99e0afd19a8d880e532c6319e2727
Oriol Fornes <oriol@cmmt.ubc.ca>
2020-05-03 18:13:53 -0700

    Update get_data.sh

--- data/get_data.sh
+++ data/get_data.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# Download i) Pfam to UniProt and ii) UniProt to PDB mappings
-# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# +++++++++++++++++++++++++++++++++++++++++++++ #
+# Download i) Pfam to UniProt                   #
+# +++++++++++++++++++++++++++++++++++++++++++++ #
 
 curl -O ftp://ftp.ebi.ac.uk/pub/databases/Pfam/current_release/Pfam-A.regions.uniprot.tsv.gz
-curl -O ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete/docs/pdbtosp.txt
 
-# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# Download iii) Reference Proteomes from UniProt
-# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# +++++++++++++++++++++++++++++++++++++++++++++ #
+# Download ii) Reference Proteomes from UniProt #
+# +++++++++++++++++++++++++++++++++++++++++++++ #
 curl -O ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/reference_proteomes/Reference_Proteomes_2020_01.tar.gz
